d7951b084a69737624ae7802a9670495ac2d4930
Narendranath Panda <3488520+naren4b@users.noreply.github.com>
2025-10-15 13:13:00 +0530

    Update varaibles.tf

--- learning/01/terraform-local/varaibles.tf
+++ learning/01/terraform-local/varaibles.tf
@@ -1,3 +1,16 @@
 variable "filename" {
  default="/tmp/temp.txt"
+ type = "string" 
+ description = "Deployment File Name "
 }
+
+variable "replicas" {
+ default="1"
+ type = "number" 
+ description = "Gives the Number of Replicas"
+}
+variable "isLocal"{
+   default ="true"
+   type = "boolean"
+   description = "Example of Boolean"
+} 
